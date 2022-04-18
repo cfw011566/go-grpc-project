c62ea3f9a08cc6f7243e8d36ffbedd7465e3bfef
CF Wang <cf.wang@metropia.com>
2022-04-18 08:43:23 +0800

    move code of api-gateway

---
+++ go-grpc-auth-svc/cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"example.com/go-grpc-auth-svc/pkg/config"
+	"example.com/go-grpc-auth-svc/pkg/db"
+	"example.com/go-grpc-auth-svc/pkg/pb"
+	"example.com/go-grpc-auth-svc/pkg/services"
+	"example.com/go-grpc-auth-svc/pkg/utils"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	jwt := utils.JwtWrapper{
+		SecretKey:       c.JWTSecretKey,
+		Issuer:          "go-grpc-auth-svc",
+		ExpirationHours: 24 * 365,
+	}
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listening:", err)
+	}
+
+	fmt.Println("Auth Svc on", c.Port)
+
+	s := services.Server{
+		H:   h,
+		Jwt: jwt,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterAuthServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
